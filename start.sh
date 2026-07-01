96fe251ca16aa41f883f8449c0171a3c0ed85884
Ark41661 <daffagans203@gmail.com>
2026-07-01 17:58:41 +0700

    fix: resolve conflict start.sh, set bash start.sh in railway.json

--- start.sh
+++ start.sh
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 #!/bin/bash
 set -e
 
-# Run database migrations
 php artisan migrate --force
 
-# Start the application using the default start container script
-exec /start-container.sh
-=======
-#!/bin/sh
-php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=$PORT
->>>>>>> 9b9a95e (fix: add start.sh, switch to SQLite, php artisan serve)
