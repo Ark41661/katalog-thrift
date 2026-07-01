45383a736f8ab7869a9675ab4c1d8cb5b26d6ae6
Ark41661 <daffagans203@gmail.com>
2026-07-01 17:15:06 +0700

    fix: add start.sh, switch to SQLite, php artisan serve

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash
 set -e
 
@@ -6,3 +7,8 @@ php artisan migrate --force
 
 # Start the application using the default start container script
 exec /start-container.sh
+=======
+#!/bin/sh
+php artisan migrate --force
+php artisan serve --host=0.0.0.0 --port=$PORT
+>>>>>>> 9b9a95e (fix: add start.sh, switch to SQLite, php artisan serve)
